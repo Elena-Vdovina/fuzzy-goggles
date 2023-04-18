ee219fdd5ebecabf427b39007289a94a78ca4b98
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-04-18 10:05:57 +0200

    Change Record

--- src/Record.java
+++ src/Record.java
@@ -1,38 +1,56 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Record {
+
   private Date date;         // дата
   private String article;    // наименование статьи расхода или дохода
   private int amount;        // сумма в центах
-  private int type;           // тип - доход или расход
-  private int category;      // тип - из справочника
+  private String type;           // тип - доход или расход
+  private String category;      // тип - из справочника
+  private static final DateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
 
-  public Record(String s, String s1, int amount, String type, String category){
-
+  public Record(String dateStr, String article, double amount, String type, String category)
+      throws ParseException {
+    setDate(dateStr);
+    this.article = article;
+    setAmount(amount);
+    this.type = type;
+    this.category = category;
   }
 
-  public void setDate(Date date) {
-    this.date = date;
+  public void setDate(String dateStr) throws ParseException {
+    this.date = formatter.parse(dateStr);
+  }
+
+  public static Date setDateF(String dateStr) throws ParseException {
+    return formatter.parse(dateStr);
   }
 
   public void setArticle(String article) {
     this.article = article;
   }
 
-  public void setAmount(int amount) {
-    this.amount = amount;
+  public void setAmount(double amount) {
+    this.amount = (int) amount * 100;
   }
 
-  public void setTyp(int typ) {
+  public void setType(String type) {
     this.type = type;
   }
 
-  public void setCategory(int category) {
+  public void setCategory(String category) {
     this.category = category;
   }
 
   public Date getDate() {
     return date;
+  }
+
+  public String getDateStr() {
+    return formatter.format(date);
   }
 
   public String getArticle() {
@@ -43,22 +61,18 @@ public class Record {
     return amount;
   }
 
-  public int getTyp() {
+  public String getType() {
     return type;
   }
 
-  public int getCategory() {
+  public String getCategory() {
     return category;
   }
 
   @Override
   public String toString() {
-    return "Record {" +
-        "date=" + date +
-        ", article='" + article + '\'' +
-        ", amount=" + amount +
-        ", type=" + type +
-        ", category=" + category +
-        '}';
+    double amountD=(double)(amount / 100);
+    return String.format("%-12s %30s %10.2f %-10s %-20s", getDateStr(), article, amountD, type,
+        category);
   }
 }
