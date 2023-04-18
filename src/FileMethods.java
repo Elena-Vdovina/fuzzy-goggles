f4219dd5217261fb14d0044ced6be0d7424f323f
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-04-19 01:04:18 +0200

    Change FileMethods

--- src/FileMethods.java
+++ src/FileMethods.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -13,18 +12,18 @@ import java.util.List;
 
 public class FileMethods {
 
-
   /**
-   * Пытаемся открыть файл
+   * Метод выбора или создания файла для работы
    *
+   * @param pathToFile имя текущего файла
    * @return pathToFile имя выбранного файла
-   * @throws IOException не обрабатывается
+   * @throws IOException    не обрабатывается
+   * @throws ParseException не обрабатывается
    */
   public static String openFile(String pathToFile) throws IOException, ParseException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     pathToFile = changeFile(pathToFile);
-    System.out.println("1 " + pathToFile);
-    while (pathToFile.isEmpty()) { // выбор или создание файла для работы
+    while (pathToFile.isEmpty()) {
       System.out.print("Имя файла не определенно. Продолжим или закончим? ");
       String command = br.readLine().toLowerCase();
       switch (command) {
@@ -35,17 +34,17 @@ public class FileMethods {
           System.exit(1);
         }
       }
-      System.out.println("1 " + pathToFile);
     }
     return pathToFile;
   }
 
   /**
-   * Открываем другой файл
+   * Метод выбора другого файла
    *
    * @param pathToFile имя текущего файла
    * @return pathStr имя выбранного файла
-   * @throws IOException не обрабатывается
+   * @throws IOException    обрабатывается
+   * @throws ParseException не обрабатывается
    */
   public static String changeFile(String pathToFile) throws IOException, ParseException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -58,33 +57,36 @@ public class FileMethods {
     try {
       Path path = Paths.get(newPath);
       boolean exists = Files.isRegularFile(path);
-      if (exists) {
+      if (exists) {                                                          // если файл существует
         String s = Files.probeContentType(path);
-        if (!s.equals("text/plain")) {
+        if (!s.equals("text/plain")) {                                     // если не текстовый файл
           System.out.println(Colors.RED + "Файл \"" + newPath +
               "\" не является текстовым!" + Colors.RESET);
-          pathStr = "";
+          if (!pathToFile.isEmpty()) {         // если были в каком-то файле, имя оставляем таким же
+            pathStr = pathToFile;
+            System.out.println("Текущий файл: " + pathToFile);
+          } else {            // создаем новое имя файла - имя нетекстового файла с расширением .txt
+            pathStr = newPath.substring(0, newPath.length() - 3) + "txt";
+            System.out.println("Создаем файл " + pathStr);
+            pathStr = newFile(pathToFile, pathStr);
+          }
         } else {
-          pathStr = newPath;
+          pathStr = newPath;                                                // берем новое имя файла
           System.out.println(newPath + " " + pathStr);
         }
-      } else {
+      } else {                                                            // если файл не существует
         System.out.println(Colors.RED + "Нет файла " + newPath + " с данными" + Colors.RESET);
-        // pathStr = newFile(pathToFile, newPath);
-        pathStr = "";
+        System.out.println("Создаем новый файл " + newPath);
+        pathStr = newFile(pathToFile, newPath);
       }
     } catch (IOException e) {
       System.out.println(e.getMessage());
-    }
-    if (pathStr.isEmpty()) {
-      System.out.println("Создать файл " + newPath + " с данными");
-      pathStr = newFile(pathToFile, newPath);
     }
     return pathStr;
   }
 
   /**
-   * Создаем новый файл
+   * Метод создает новый файл
    *
    * @param pathToFile имя текущего файла
    * @param newPath    имя нового файла
@@ -94,58 +96,84 @@ public class FileMethods {
   public static String newFile(String pathToFile, String newPath)
       throws IOException, ParseException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String pathStr = "";
-    System.out.println(pathToFile + " " + newPath);
-    if (!pathToFile.isEmpty()) {
-      System.out.println("1 - Создать список дел в файле " + newPath);
-      System.out.println("2 - Создать новый список дел в файле " + pathToFile);
+    if (!pathToFile.isEmpty()) {                                             //если был текущий файл
+      System.out.println("1 - Создать список в файле " + newPath);
+      System.out.println("2 - Создать новый список в файле " + pathToFile);
       System.out.println("3 - Вернуться в меню и остаться в файле ");
       String file = (br.readLine());
       while (!(file.equals("1") || file.equals("2") || file.equals("3"))) {
-        // проверка на соответствующее значение
         System.out.print(Colors.RED + "Некорректное значение. Попробуйте еще раз: " + Colors.RESET);
         file = br.readLine();
       }
       switch (file) {
-        case "1" -> {  // создать список дел в новом файле
-          createNewList(newPath);
-          pathStr = newPath;
+        case "1" -> {                                                // создать список в новом файле
+          MenuMethods.pathToFile_ = newPath;
+          RecordMethods.records = createNewList();
+          FileMethods.writeFile(MenuMethods.pathToFile_, RecordMethods.records);
         }
-        case "2" -> {   // создать новый список дел в текущем файле
-          createNewList(pathToFile);
-          pathStr = pathToFile;
+        case "2" -> {                                        // создать новый список в текущем файле
+          MenuMethods.pathToFile_ = pathToFile;
+          RecordMethods.records = createNewList();
+          FileMethods.writeFile(MenuMethods.pathToFile_, RecordMethods.records);
         }
-        case "3" -> pathStr = pathToFile;  // вернуться в меню с текущим файлом
+        case "3" -> MenuMethods.pathToFile_ = pathToFile;       // вернуться в меню с текущим файлом
       }
-    } else {
-      createNewList(newPath);
-      pathStr = newPath;
+    } else {                                                         // при первом входе в программу
+      MenuMethods.pathToFile_ = newPath;
+      RecordMethods.records = createNewList();
+      FileMethods.writeFile(MenuMethods.pathToFile_, RecordMethods.records);
     }
-    return pathStr;
+    return MenuMethods.pathToFile_;
   }
 
-  public static void createNewList(String pathToFile) throws IOException, ParseException {
+  /**
+   * Метод создает новый список в файле
+   *
+   * @return List<Record> records возвращает созданный список
+   * @throws IOException    не обрабатывается
+   * @throws ParseException не обрабатывается
+   */
+  public static List<Record> createNewList() throws IOException, ParseException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    System.out.println("Создание нового списка дел: ");
-    List<java.lang.Record> records = new ArrayList<>();
-    // прочитали
+    System.out.println("Создание нового списка: ");
+    List<Record> records = new ArrayList<>();
     int i = 1;
-    while (i == 1) { //
+    while (i == 1) {
       System.out.println();
-      RecordMethods.addRecord();
+      System.out.println("Новая запись:");
+      System.out.print("Дата (\"ДД.ММ.ГГГГ\") - ");
+      String dateStr = RecordMethods.dateValidation(br);
+      System.out.print("Содержание ");
+      String article = br.readLine();
+      while (article.isEmpty()) {
+        System.out.println(Colors.RED + "Содержание не может быть пустым:" + Colors.RESET);
+        article = br.readLine();
+      }
+      System.out.print("Сумма ");
+      double amountD = Double.parseDouble(br.readLine());
+      while (amountD <= 0) {
+        System.out.println(Colors.RED + "Сумма должна быть >0: " + Colors.RESET);
+        amountD = Double.parseDouble(br.readLine());
+      }
+      int amount = (int) amountD * 100;
+      System.out.print("Доход/расход: ");
+      String type = br.readLine();
+      System.out.print("Категория: ");
+      String category = br.readLine();
+      Record record = new Record(dateStr, article, amount, type, category);
+      records.add(record);
       System.out.print("Добавить новую запись (1-да, 2-выход): ");
       i = Integer.parseInt(br.readLine());
     }
-    writeFile();
-    //printList(pathToFile);
+    return records;
   }
 
   /**
    * Метод чтения данных из файла
    *
-   * @return List<Record> records лист с записями
-   * @throws IOException               если файла с записями не существует
-   * @throws IndexOutOfBoundsException если файл с записями пустой
+   * @return List<Record> records лист со списком
+   * @throws IOException               если файла со списком не существует
+   * @throws IndexOutOfBoundsException если файл со списком пустой
    */
   public static List<Record> readFile() throws IOException, ParseException {
     List<Record> records = new ArrayList<>();
@@ -156,38 +184,35 @@ public class FileMethods {
       while ((line = fr.readLine()) != null) {
         lines.add(line);
       }
-      System.out.println(lines);
-
       for (String s : lines) {
         List<String> columns = List.of(s.split(";", -1));
         int amount = Integer.parseInt(columns.get(2));
         Record record = new Record(columns.get(0), columns.get(1), amount, columns.get(3),
             columns.get(4));
         records.add(record);
-        System.out.println(record);
       }
-
       fr.close();
     } catch (IOException e) {
-      System.out.println("У Вас не обнаружен список дел");
-      createNewList(MenuMethods.pathToFile_);
-      //} catch (ParseException e) {
-      //  throw new RuntimeException(e);
-    } catch (IndexOutOfBoundsException e) {
-      System.out.println(Colors.RED + "В текущем файле нет списка дел!" + Colors.RESET);
-      createNewList(MenuMethods.pathToFile_);
+      System.out.println("У Вас не обнаружен списка");
+      RecordMethods.records = createNewList();
+      FileMethods.writeFile(MenuMethods.pathToFile_, RecordMethods.records);
+     } catch (IndexOutOfBoundsException e) {
+      System.out.println(Colors.RED + "В текущем файле нет списка!" + Colors.RESET);
+      RecordMethods.records = createNewList();
+      FileMethods.writeFile(MenuMethods.pathToFile_, RecordMethods.records);
     }
     return records;
   }
 
   /**
-   * Метод записи List<Record> records в файл
-   *
+   * Метод записывает лист списка в файл
+   * @param pathToFile имя текущего файла
+   * @param records лист списка
    * @throws IOException не обрабатывается
    */
-  public static void writeFile() throws IOException {
-    FileWriter fr = new FileWriter(MenuMethods.pathToFile_);
-    for (Record record : RecordMethods.records) {
+  public static void writeFile(String pathToFile, List<Record> records) throws IOException {
+    FileWriter fr = new FileWriter(pathToFile);
+    for (Record record : records) {
       fr.write(record.toStringFile());
     }
     fr.close();
