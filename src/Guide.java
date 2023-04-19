52d18a41fd6f28bb00b3d2aca5597084affb1d29
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-04-19 02:05:27 +0200

    Change Guide

--- src/Guide.java
+++ src/Guide.java
@@ -1,85 +1,131 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Guide {
 
-  final public static char SEP = ';';
   public static List<String> income;
   public static List<String> expenses;
 
-  public static List<String> readGuideI() {
+  /**
+   * Метод читает справочник доходов из файла
+   *
+   * @return income лист доходов
+   * @throws IOException не обрабатывается
+   */
+  public static List<String> readGuideI() throws IOException {
     List<String> income = new ArrayList<>();
-    income.add("Аванс");
-    income.add("Зарплата");
-    income.add("Проценты");
-    income.add("Гонорар");
-    income.add("Подарки");
-    income.add("Пособие");
+    File pathGuide = new File("res/income.txt");                   // создали путь к файлу
+    if (!pathGuide.exists()) {
+      pathGuide.createNewFile();                                       // создали файл, если его нет
+    }
+    BufferedReader br = new BufferedReader(new FileReader(pathGuide));
+    for (String line = br.readLine(); line != null; line = br.readLine()) {
+      income.add(line);
+    }
+    br.close();
     return income;
   }
 
-  public static List<String> readGuideE() {
+  /**
+   * Метод читает справочник расходов из файла
+   *
+   * @return expenses лист расходов
+   * @throws IOException не обрабатывается
+   */
+  public static List<String> readGuideE() throws IOException {
     List<String> expenses = new ArrayList<>();
-    expenses.add("Коммунальные платежи");
-    expenses.add("Продукты");
-    expenses.add("Одежда");
-    expenses.add("Транспорт");
-    expenses.add("Бытовая химия");
-    expenses.add("Аптека");
-    expenses.add("Обучение");
-    expenses.add("Игрушки");
-    expenses.add("Канцелярия");
-    expenses.add("Домашние животные");
-    expenses.add("Жилье");
-    expenses.add("Ремонт");
-    expenses.add("Техника");
-    expenses.add("Мебель");
-    expenses.add("Развлечения");
-    expenses.add("Связь");
-    expenses.add("Копилка");
-    expenses.add("Инвестиции");
+    File pathGuide = new File("res/expenses.txt");                 // создали путь к файлу
+    if (!pathGuide.exists()) {
+      pathGuide.createNewFile();                                       // создали файл, если его нет
+    }
+    BufferedReader br = new BufferedReader(new FileReader(pathGuide));
+    for (String line = br.readLine(); line != null; line = br.readLine()) {
+      expenses.add(line);
+    }
+    br.close();
     return expenses;
   }
-/*
-    public static void main(String[] args) throws IOException {
-      File phoneBook = new File("res/phonebook.txt"); // создали путь к файлу
-      if (!phoneBook.exists()) { // файла ещё нет - первый запуск программы
-        phoneBook.createNewFile(); // создали файл, если его нет
-      }
 
-      List<Map<String, String>> records = new ArrayList<>();
-      BufferedReader br = new BufferedReader(new FileReader(phoneBook)); // открыли файл для чтения
-      // читаем строчки файла, пока они есть
-      // если строчек нет или они закончились, readLine() вернёт null
-      // курсор в файле всегда стоит в конце уже прочитанных данных
-      for (String line = br.readLine(); line != null; line = br.readLine()) {
-        int lastSep = line.lastIndexOf(SEP); // последнее появление разделителя
-        // прочитал из файла строку вида "имя;номер телефона", ';' - это SEP
-        String name = line.substring(0, lastSep); // имя до разделителя
-        String phone = line.substring(lastSep + 1); // номер телефона после разделителя
-
-        Map<String, String> entry = new HashMap<>();
-        entry.put("name", name);
-        entry.put("phone", phone);
-        // создал запись - словарь {"name": "имя", "phone": "номер телефона"}
-        records.add(entry); // добавил эту запись в список записей
-      }
-      br.close(); // закрываю файл после чтения
-
-      // Что-то делаю с записями или даже добавляю новые
-      Map<String, String> newEntry = new HashMap<>();
-      newEntry.put("name", "Name"); // создал запись - словарь {"имя": "номер телефона"}
-      newEntry.put("phone", "Phone"); // создал запись - словарь {"имя": "номер телефона"}
-      records.add(newEntry); // добавил эту запись в список записей
-
-      FileWriter fileWriter = new FileWriter(phoneBook); // открываю этот файл теперь для записи
-      for (Map<String, String> entry : records) {
-        String name = entry.get("name");
-        String phone = entry.get("phone");
-        fileWriter.write(name + SEP + phone + "\n");
-      }
-      fileWriter.close(); // закрываем файл после записи
+  /**
+   * Метод печатает лист доходов на экран
+   */
+  public static void printGuideI() {
+    System.out.println();
+    System.out.println("Справочник доходов");
+    int i = 0;
+    for (String s : income) {
+      System.out.println(i + 1 + " " + s);
+      ++i;
     }
+  }
 
- */
+  /**
+   * Метод добавляет новый вид дохода в справочник
+   *
+   * @throws IOException не обрабатывается
+   */
+  public static void addGuideI() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println();
+    System.out.print("Новый вид дохода: ");
+    String line = br.readLine();
+    while (line.isEmpty()) {
+      System.out.println(Colors.RED + "Содержание не может быть пустым:" + Colors.RESET);
+      line = br.readLine();
+    }
+    income.add(line);
+    writeGuide("res/income.txt", income);
+  }
+
+  /**
+   * Метод печатает расходов на экран
+   */
+  public static void printGuideE() {
+    System.out.println();
+    System.out.println("Справочник расходов");
+    int i = 0;
+    for (String s : expenses) {
+      System.out.println(i + 1 + " " + s);
+      ++i;
+    }
+  }
+
+  /**
+   * Метод добавляет новый вид расходов в справочник
+   *
+   * @throws IOException не обрабатывается
+   */
+  public static void addGuideE() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println();
+    System.out.print("Новый вид расходов: ");
+    String line = br.readLine();
+    while (line.isEmpty()) {
+      System.out.println(Colors.RED + "Содержание не может быть пустым:" + Colors.RESET);
+      line = br.readLine();
+    }
+    expenses.add(line);
+    writeGuide("res/income.txt", expenses);
+  }
+
+  /**
+   * Метод записывает справочник в файл
+   *
+   * @param pathGuide имя файла справочника
+   * @param guide лист справочника
+   * @throws IOException не обрабатывается
+   */
+  public static void writeGuide(String pathGuide, List<String> guide) throws IOException {
+    FileWriter fileWriter = new FileWriter(pathGuide);
+    for (String s : guide) {
+      fileWriter.write(s + "\n");
+    }
+    fileWriter.close();
+  }
 }
