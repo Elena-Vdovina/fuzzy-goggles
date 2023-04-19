7503797bf70e126802ab9b9d7accd51e0c5d4683
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-04-19 19:38:52 +0200

    Change Main

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,17 @@ import java.text.ParseException;
 
 public class Main {
 
-
+  /**
+   * Точка входа в программу
+   *
+   * @param args список аргументов
+   * @throws IOException    не обрабатывается
+   * @throws ParseException не обрабатывается
+   */
   public static void main(String[] args) throws IOException, ParseException {
     MenuMethods.pathToFile_ = "";
     // определяем файл пользователя
     MenuMethods.pathToFile_ = FileMethods.openFile(MenuMethods.pathToFile_);
     MenuMethods.menu();  // вход в основное меню
   }
-
 }
-
-/*
-01.03.2023;Расход;22201;expenses;Обучение
-29.03.2023;Расход;100201;income;Аванс
-01.04.2023;Расход;200;expenses;Транспорт
-03.04.2023;Расход;40000;expenses;Жилье
-06.04.2023;Расход;500;expenses;Ремонт
-10.04.2023;Расход;400000;income;Зарплата
-12.04.2023;Расход;15000;expenses;Продукты
- */
