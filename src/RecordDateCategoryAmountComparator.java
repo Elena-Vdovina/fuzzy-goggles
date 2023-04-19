080036c1d427dccd87b2e6c2650a1e008235a32c
Elena Sergienko <marketing.seo.web2020@gmail.com>
2023-04-19 13:32:56 +0300

    Change Comparator

--- src/RecordDateCategoryAmountComparator.java
+++ src/RecordDateCategoryAmountComparator.java
@@ -1,13 +1,13 @@
 import java.util.Comparator;
 
-public class RecordDateArticleAmountComparator implements Comparator<Record> {
+public class RecordDateCategoryAmountComparator implements Comparator<Record> {
 
   @Override
   public int compare(Record o1, Record o2) {
     if (!o1.getDate().equals(o2.getDate())) {
       return o1.getDate().compareTo(o2.getDate());
-    } else if (!o1.getArticle().equals(o2.getArticle())) {
-      return o1.getArticle().compareTo(o2.getArticle());
+    } else if (!o1.getCategory().equals(o2.getCategory())) {
+      return o1.getCategory().compareTo(o2.getCategory());
     } else {
       return o1.getAmount() - (o2.getAmount());
     }
